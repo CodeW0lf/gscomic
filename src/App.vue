71e3d7bf19d43c1eb26011cbdb2072d51294648b
Kevin <lonewolf384@gmail.com>
2020-05-10 13:15:52 -0700

    Fix Date

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
         <p><b>Posting schedule resumed! <a href="https://www.patreon.com/posts/32457373">Read more here</a></b>.<br/>
           Like the comic? Support us on
           <a href="https://www.patreon.com/elitetrick" class="has-text-patreon" target="_blank"><i class="fab fa-patreon"></i> Patreon</a></p>
-        <p>Copyright &copy; 2018-2019 Steven Lloyd. All rights reserved</p>
+        <p>Copyright &copy; {{ new Date().getFullYear() }} Steven Lloyd. All rights reserved</p>
       </div>
     </footer>
   </div>
