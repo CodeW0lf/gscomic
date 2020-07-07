2918a0e4f7cc62aeaa2f7675d26b5df1fff0c1b1
Kevin <lonewolf384@gmail.com>
2020-07-06 17:05:35 -0700

    Update deps.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,32 @@ module.exports = {
     './src/**/*.html',
     './src/**/*.vue',
     './src/**/*.jsx',
-    './public/*.html'
+    './public/*.html',
   ],
   theme: {
     fontFamily: {
-      body: ['Roboto', 'sans-serif']
+      body: ['Roboto', 'sans-serif'],
     },
     extend: {
       colors: {
         patreon: {
           default: '#9a473a',
-          lighter: '#F96854'
+          lighter: '#F96854',
         },
         primary: '#3B9C7D',
         discord: {
           default: '#7289da',
-          lighter: '#99aab5'
+          lighter: '#99aab5',
         },
         twitter: {
           default: '#1B95E0',
-          lighter: '#99aab5'
-        }
-      }
+          lighter: '#99aab5',
+        },
+      },
     },
   },
   variants: {
-    textColor: ['responsive', 'hover', 'focus', 'disabled']
+    textColor: ['responsive', 'hover', 'focus', 'disabled'],
   },
   plugins: [],
 }
