189f57cbb75c81cb9fe974446068278d8b9f4b67
Kevin <lonewolf384@gmail.com>
2021-02-15 12:28:17 -0800

    Add explicit footer component

--- src/App.vue
+++ src/App.vue
@@ -26,37 +26,9 @@
     >
       <site-nav></site-nav>
       <router-view />
-      <SocialLinks></SocialLinks>
+      <social-links></social-links>
       <footer class="text-gray-400 mt-16 text-center py-2">
-        <p class="font-semibold">
-          Comic updates (most) Wednesdays. <br class="sm:hidden" />Sketches
-          posted Friday.
-        </p>
-        <p>
-          Like the comic? Support me on
-          <a
-            href="https://www.patreon.com/elitetrick"
-            target="_blank"
-            class="text-patreon hover:text-white"
-          >
-            <svg
-              class="fill-current inline-block w-4 h-4 align-text-top"
-              role="img"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Patreon icon</title>
-              <path
-                d="M15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613C24 4.4 20.136.524 15.386.524M.003 23.537h4.22V.524H.003"
-              />
-            </svg>
-            Patreon
-          </a>
-        </p>
-        <p class="mt-8 mb-4">
-          Copyright &copy; {{ new Date().getFullYear() }} Steven Lloyd. All
-          rights reserved
-        </p>
+        <site-footer />
       </footer>
     </div>
   </div>
@@ -65,14 +37,14 @@
 <script>
 import SiteNav from '@/components/SiteNav'
 import SocialLinks from '@/components/SocialLinks'
+import SiteFooter from '@/SiteFooter'
 
 export default {
   name: 'app',
   components: {
+    SiteFooter,
     SiteNav,
     SocialLinks,
   },
 }
 </script>
-
-<style scoped></style>
---
+++ src/SiteFooter.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p class="font-semibold">
+      Comic updates (most) Wednesdays. <br class="sm:hidden" />Sketches posted
+      Friday.
+    </p>
+    <p>
+      Like the comic? Support me on
+      <a
+        href="https://www.patreon.com/elitetrick"
+        target="_blank"
+        class="text-patreon hover:text-white"
+      >
+        <svg
+          class="fill-current inline-block w-4 h-4 align-text-top"
+          role="img"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Patreon icon</title>
+          <path
+            d="M15.386.524c-4.764 0-8.64 3.876-8.64 8.64 0 4.75 3.876 8.613 8.64 8.613 4.75 0 8.614-3.864 8.614-8.613C24 4.4 20.136.524 15.386.524M.003 23.537h4.22V.524H.003"
+          />
+        </svg>
+        Patreon
+      </a>
+    </p>
+    <p class="mt-8 mb-4">
+      Copyright &copy; {{ new Date().getFullYear() }} Steven Lloyd. All rights
+      reserved
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'site-footer',
+}
+</script>
