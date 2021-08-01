28f3200cb423d2d3533a5a564f9e34c4b413c91e
Kevin <lonewolf384@gmail.com>
2021-07-31 21:46:32 -0700

    Finalize v1 of character page.

--- src/components/lore/LoreHome.vue
+++ src/components/lore/LoreHome.vue
@@ -1,31 +1,26 @@
 <template>
   <section
-    class="grid grid-cols-1 gap-8 justify-center items-center mb-4 md:mt-8"
+      class="grid grid-cols-1 gap-8 justify-center items-center mb-4 md:mt-8"
   >
     <section-button
-      path="/lore/places"
-      :img="require('@/assets/lore/lore-locations-btn.png')"
-      alt="Location Lore"
+        path="/lore/places"
+        :img="require('@/assets/lore/lore-locations-btn.png')"
+        alt="Location Lore"
     />
     <section-button
-      path="/lore/culture"
-      alt="Culture Lore"
-      :img="require('@/assets/lore/lore-culture.png')"
-    />
-    <section-button
-      path="/lore"
-      :img="require('@/assets/lore/lore-characters.png')"
-      alt="Character Lore"
-      disabled
+        path="/lore/culture"
+        alt="Culture Lore"
+        :img="require('@/assets/lore/lore-culture.png')"
     />
   </section>
 </template>
 
 <script>
 import SectionButton from '@/components/lore/SectionButton'
+
 export default {
   name: 'LoreHome',
-  components: { SectionButton },
+  components: {SectionButton},
 }
 </script>
 
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,23 +1,68 @@
 <template>
-  <section class="mt-2 pt-8 text-gray-400 w-full border-t border-gray-700">
+  <section class="mt-2 pt-8 text-gray-400 w-full">
     <div class="flex">
       <div class="flex flex-col justify-between items-center px-4 w-full">
         <div class="">
           <h1 class="text-primary text-5xl font-bold">Kiva</h1>
         </div>
         <div class="w-3/4">
-          <img alt="Character Image" :src="require('@/assets/characters/' + character.fullImg)">
+          <div class="flex flex-col w-full">
+            <div>
+              <transition mode="out-in" :enter-active-class="'animated fade-duration ' + enterActive"
+                          :leave-active-class="'animated fade-duration ' + leaveActive">
+                <img alt="Character Image" :src="require('@/assets/characters/' + character.fullImg)">
+              </transition>
+            </div>
+            <div class="flex justify-between mx-auto -mt-20 w-full py-8">
+              <div>
+                <button disabled title="More Coming Soon!"
+                        class="focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50 rounded disabled:text-gray-700 text-primary">
+                  <svg aria-hidden="true" focusable="false"
+                       class="fillCurrent w-8 h-8 sm:w-10 sm:h-10 transform rotate-180" role="img"
+                       xmlns="http://www.w3.org/2000/svg"
+                       viewBox="0 0 448 512">
+                    <!--
+                    Font Awesome Free 5.15.0 by @fontawesome - https://fontawesome.com
+                    License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+                    -->
+                    <path fill="currentColor"
+                          d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <button disabled title="More Coming Soon!"
+                        class="focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50 rounded disabled:text-gray-700 text-primary">
+                  <svg aria-hidden="true" focusable="false"
+                       class="fillCurrent w-8 h-8 sm:w-10 sm:h-10" role="img"
+                       xmlns="http://www.w3.org/2000/svg"
+                       viewBox="0 0 448 512">
+                    <!--
+                    Font Awesome Free 5.15.0 by @fontawesome - https://fontawesome.com
+                    License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+                    -->
+                    <path fill="currentColor"
+                          d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div class="flex justify-center w-full">
-          <div class="w-1/3 self-end">
+          <div class="w-1/3 self-end overflow-hidden">
             <div class="flex flex-col w-full">
               <div>
-                <img alt="Character Portrait" :src="require('@/assets/characters/' + portrait.portraitImg)">
+                <transition mode="out-in" :enter-active-class="'animated fade-duration ' + enterActive"
+                            :leave-active-class="'animated fade-duration ' + leaveActive">
+                  <img alt="Character Portrait" :key="portrait.portraitImg" class="z-0"
+                       :src="require('@/assets/characters/' + portrait.portraitImg)">
+                </transition>
               </div>
-              <div class="flex justify-between mx-auto -mt-8 w-full sm:px-4">
+              <div class="flex justify-between mx-auto -mt-8 w-full sm:px-4 z-10">
                 <div>
-                  <button @click="prevPortrait"
+                  <button @click="prevPortrait" title="See what others have to say!"
                           class="focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50 rounded disabled:text-gray-700">
                     <svg aria-hidden="true" focusable="false"
                          class="fillCurrent w-8 h-8 sm:w-10 sm:h-10 text-primary transform rotate-180" role="img"
@@ -33,7 +78,7 @@
                   </button>
                 </div>
                 <div>
-                  <button @click="nextPortrait"
+                  <button @click="nextPortrait" title="See what others have to say!"
                           class="focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50 rounded disabled:text-gray-700">
                     <svg aria-hidden="true" focusable="false"
                          class="fillCurrent w-8 h-8 sm:w-10 sm:h-10 text-primary" role="img"
@@ -52,8 +97,12 @@
             </div>
           </div>
           <div class="triangle-border left mb-4 sm:mb-14 p-2 h-96 w-2/3">
-            <div class="prose p-4 overflow-y-auto text-center h-full w-full" v-html="portrait.text">
-            </div>
+            <transition mode="out-in" enter-active-class="animated fadeIn fade-duration"
+                        leave-active-class="animated fadeOut fade-duration">
+              <div class="prose p-4 overflow-y-auto text-center h-full w-full"
+                   v-html="portrait.text" :key="portrait.text">
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -63,68 +112,17 @@
 </template>
 
 <script>
+import characterList from '@/assets/characters/characterList.json';
+
 export default {
   name: "Characters",
   data() {
     return {
       selectedCharIdx: 0,
       selectedPortraitIdx: 0,
-      characterList: [
-        {
-          fullImg: 'kiva_full.png',
-          talkingHeads: [
-            {
-              portraitImg: 'kiva_portrait.png',
-              text: 'I am Kiva\'s text'
-            },
-            {
-              portraitImg: 'max_portrait.png',
-              text: '<p>EEEEEEEEEE!!! That’s Kiva! Oh, man, you guys, he’s SO COOL. And he’s sweet and kind and soft and\n' +
-                  '                handsome\n' +
-                  '                and his spots are SO BEAUTIFUL and he’s just the greatest guy EVER.</p>\n' +
-                  '              <p>Did I ever tell you guys how I met him? OMG, so get this okay? So I’m walking along, and… um… cuz some\n' +
-                  '                stuff just happened, and I’m feeling kinda down, right? And I see him digging through a pile of stuff,\n' +
-                  '                and\n' +
-                  '                my eyes just fall on his back, cuz like… I’ve never seen anyone with so many spots before! Like… okay,\n' +
-                  '                he\n' +
-                  '                had his shirt off… And I’m sitting there staring, and it was probably kinda rude, but he finally notices\n' +
-                  '                me\n' +
-                  '                and he whirls around and steps up all threatening like, right? And then he’s like, “WHAT ARE YOU STARING\n' +
-                  '                AT?” in his big scary Kiva voice, you know? So, I kinda just break down and start crying, because at\n' +
-                  '                that\n' +
-                  '                point I’m terrified he’s gonna hurt me, cuz I didn’t know him, and cuz that’s like… anyway... So instead\n' +
-                  '                he\n' +
-                  '                feels really bad about it, and he just sits down with me and we start talking and I just prolly go on\n' +
-                  '                and\n' +
-                  '                on\n' +
-                  '                for hours, spilling my whole life’s story to him and he just kinda… listens. And see… nobody’s ever done\n' +
-                  '                that before… or since, actually… And when I’m done crying and talking I look at him, and I just get lost\n' +
-                  '                into his big beautiful eyes, and I just kinda… lean in to kiss him. But then he pushes me over, cuz\n' +
-                  '                like…\n' +
-                  '                yeah, it was a bit much, I’ll admit. I don’t normally… DO stuff like that… but it was just… I dunno!\n' +
-                  '                Anyway,\n' +
-                  '                after that, he walks with me back to the Twirl, cuz like… Kiva was MAD after all that stuff I told him,\n' +
-                  '                and\n' +
-                  '                he was about to punch someone I think… but by then Gilly’s calmed down and he’s being really nice to me\n' +
-                  '                all\n' +
-                  '                of a sudden? And he kinda pulled me off to the side and Kiva didn’t want me to go with him, but I did\n' +
-                  '                cuz\n' +
-                  '                like… he had all my stuff… and like… it sounded like a pretty good deal the way he was going on about\n' +
-                  '                it…\n' +
-                  '                so… that’s… a whole thing now… But anyway, Kiva and I kept kinda meeting up every once in awhile, just\n' +
-                  '                for\n' +
-                  '                like, going out for food and drinks and stuff… Before we knew it, we were hanging out almost every day!\n' +
-                  '                Usually for drinks… and well, I don’t drink, but Kiva does, he drinks a LOT. Wait… I shouldn’t really…\n' +
-                  '                um...</p>\n' +
-                  '              <p>Anyway, Kiva is awesome, I’ve seen him fight a couple of times, he’s AMAZING! He kicks the tails of\n' +
-                  '                anyone\n' +
-                  '                who steps up… if… er… when he’s sober. And I just… I dunno… I feel really safe when he’s around. Not\n' +
-                  '                like…\n' +
-                  '                um… well, he makes me feel safe. That’s all.</p>'
-            }
-          ]
-        }
-      ]
+      characterList: characterList,
+      enterActive: 'slideInRight',
+      leaveActive: 'slideOutLeft',
     }
   },
   computed: {
@@ -137,9 +135,13 @@ export default {
   },
   methods: {
     nextPortrait() {
+      this.enterActive = 'slideInRight'
+      this.leaveActive = 'slideOutLeft'
       this.selectedPortraitIdx = ++this.selectedPortraitIdx % this.character.talkingHeads.length;
     },
     prevPortrait() {
+      this.enterActive = 'slideInLeft'
+      this.leaveActive = 'slideOutRight'
       this.selectedPortraitIdx = Math.abs(--this.selectedPortraitIdx % this.character.talkingHeads.length);
     }
   }
@@ -232,5 +234,9 @@ export default {
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
   background: #57e7b9;
+}
+
+.fade-duration {
+  animation-duration: 250ms;
 }
 </style>
