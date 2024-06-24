85028d1738f5fc818976384065824375099cb387
Kevin Lloyd <lonewolf384@gmail.com>
2024-06-23 17:12:49 -0700

    Fix API service so it works in production with any domain

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,7 +4,7 @@ export default () => {
   const baseURL =
     import.meta.env.DEV
       ? 'http://localhost:8088/'
-      : 'https://www.godslayerscomic.com/api'
+      : '/api/'
   return axios.create({
     baseURL,
   })
--- src/services/ComicsService.js
+++ src/services/ComicsService.js
@@ -2,15 +2,15 @@ import Api from '@/services/Api'
 
 export default {
   getComics() {
-    return Api().get('get-comics.php')
+    return Api().get('/get-comics.php')
   },
   getRileyComics(version) {
-    return Api().get('get-riley-comics.php?version=' + version)
+    return Api().get('/get-riley-comics.php?version=' + version)
   },
   getSketches() {
-    return Api().get('get-sketches.php')
+    return Api().get('/get-sketches.php')
   },
   getBadges() {
-    return Api().get('get-badges.php')
+    return Api().get('/get-badges.php')
   }
 }
