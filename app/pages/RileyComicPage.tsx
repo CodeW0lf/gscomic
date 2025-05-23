01e278a5bc22a9d1e7d587e8175c257e8617256f
Kevin Lloyd <lonewolf384@gmail.com>
2025-05-23 00:18:57 -0700

    inline version switcher

--- app/pages/RileyComicPage.tsx
+++ app/pages/RileyComicPage.tsx
@@ -14,40 +14,38 @@ export function RileyComicPage() {
     setVersion(e.target.checked ? 'b' : 'a');
   };
 
-  const versionSwitcher = (
-    <div className="flex flex-col items-center justify-center">
-      <div className="switch-button">
-        <input
-          className="switch-button-checkbox"
-          id="switch"
-          type="checkbox"
-          checked={isSecondViewSelected}
-          onChange={switchValue}
-        />
-        <label className="switch-button-label" htmlFor="switch">
-          <span className="switch-button-label-span">
-            <AnimatePresence>
-              {!isSecondViewSelected && (
-                <motion.img
-                  key="grin"
-                  src={grinImg}
-                  alt="Riley Grin"
-                  className="mx-auto"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1, transition: { duration: 0.3 } }}
-                  exit={{ opacity: 0, transition: { duration: 0.1 } }}
-                />
-              )}
-            </AnimatePresence>
-          </span>
-        </label>
-      </div>
-    </div>
-  );
-
   return (
     <ComicNavProvider comicPath="rileycomic" version={version}>
-      <ComicWrapper>{versionSwitcher}</ComicWrapper>
+      <ComicWrapper>
+        <div className="flex flex-col items-center justify-center">
+          <div className="switch-button">
+            <input
+              className="switch-button-checkbox"
+              id="switch"
+              type="checkbox"
+              checked={isSecondViewSelected}
+              onChange={switchValue}
+            />
+            <label className="switch-button-label" htmlFor="switch">
+              <span className="switch-button-label-span">
+                <AnimatePresence>
+                  {!isSecondViewSelected && (
+                    <motion.img
+                      key="grin"
+                      src={grinImg}
+                      alt="Riley Grin"
+                      className="mx-auto"
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1, transition: { duration: 0.3 } }}
+                      exit={{ opacity: 0, transition: { duration: 0.1 } }}
+                    />
+                  )}
+                </AnimatePresence>
+              </span>
+            </label>
+          </div>
+        </div>
+      </ComicWrapper>
     </ComicNavProvider>
   );
 }
